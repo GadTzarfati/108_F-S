14d9ac786d67236a0135a45d0bb7bcc7575eeb97
2552123456 <vidal.judea@gmail.com>
2024-12-01 12:02:38 +0200

    Connecting to Mongodb

--- 108_Server/server.js
+++ 108_Server/server.js
@@ -1,16 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import userRouter from './routes/userRoutes.js';
+
+dotenv.config(); 
 
 const app = express();
 
-// Middleware לטיפול בנתונים בפורמט JSON
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
+
 app.use(express.json());
 
-// חיבור הנתיב
 app.use('/api/users', userRouter);
 
-// הפעלת השרת
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
