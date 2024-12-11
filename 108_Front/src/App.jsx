381829cc137c22cddbcea9560983254294477865
GadTzarfati <gadtzarfati26@gmail.com>
2024-12-11 14:54:49 +0200

    dth

--- 108_Front/src/App.jsx
+++ 108_Front/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import AppRouter from './AppRouter'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <AppRouter />
   )
 }
 
-export default App
+export default App;
